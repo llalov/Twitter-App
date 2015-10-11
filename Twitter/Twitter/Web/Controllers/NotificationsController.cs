9dca09d14ff86505713b3e33454709461cc8d4c2
Lachezar Lalov <luchoazz@abv.bg>
2015-10-11 14:10:04 +0300

    Added notifications controller

---
+++ Twitter/Twitter/Web/Controllers/NotificationsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Twitter.Data;
+
+namespace Web.Controllers
+{
+    public class NotificationsController : BaseController
+    {
+        protected NotificationsController(ITwitterData data) : base(data)
+        {
+        }
+    }
+}
