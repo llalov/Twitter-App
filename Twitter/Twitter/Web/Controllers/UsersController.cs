8ff36367549b0171ac0b0c9de8fd04ea73af1978
llalov <luchoazz@abv.bg>
2015-10-27 02:48:37 +0200

    added live search for users

--- Twitter/Twitter/Web/Controllers/UsersController.cs
+++ Twitter/Twitter/Web/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Web.Models.ViewModels;
@@ -11,12 +12,24 @@ namespace Web.Controllers
     public class UsersController : BaseController
     {
         // GET: Users
-        [Authorize]
-        [HttpGet]
-        public ActionResult AllUsers()
+        [System.Web.Mvc.Authorize]
+        [System.Web.Mvc.HttpGet]
+        public ActionResult Index()
         {
             var users = this.Data.Users.All();
             return View(users);
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Authorize]
+        public ActionResult Search([FromUri]string query)
+        {
+            var resutlWords = Data.Users
+                .All()
+                .Where(u => u.FullName.StartsWith(query))
+                .Select(UserViewModel.Create)
+                .ToList();
+            return this.Json(resutlWords, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Twitter/Twitter/Web/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using Twitter.Data.Models;
+
+namespace Web.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public static Expression<Func<User, UserViewModel>> Create
+        {
+            get
+            {
+                return user => new UserViewModel()
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    AvatarUrl = user.AvatarUrl
+                };
+            }
+        }
+    }
+}
